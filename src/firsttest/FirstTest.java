f388fa66c08fb8d856e99749fd263eb85f9fc8df
dzcorovic <dzenanacorovic@yahoo.com>
2020-06-10 18:52:01 +0200

    This is my Java project.

---
+++ src/firsttest/FirstTest.java
@@ -0,0 +1,12 @@
+package firsttest;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+    // print in console some text
+		
+		System.out.println("Dobrodosli u Nahlu");
+		System.out.print("Dobrodosli u Nahlu");
+	}
+
+}
---
+++ src/secondtest/SecondTest.java
@@ -0,0 +1,14 @@
+package secondtest;
+
+public class SecondTest {
+
+	public static void main(String[] args) {
+		int number=11;
+		if (number>10) {
+        System.out.println("Vecina ucenika je dosla. Cas moze poceti");
+		} else {
+		System.out.println("Nedovoljan broj ucenika za odrzavanje casa");
+		}
+}
+
+}	
